a55da0b581db1e0a05bbd92806b7c44a44e8061d
edeister <deister.ethan@student.greenriver.edu>
2024-05-13 01:43:56 -0400

    Fixed composer.json authors array, added Listing class, added example instrument/listing images, added a dynamic include page which displays instrument listings as bootstrap cards in rows (get-listings.html), included get-listings in search.html and defined an example route for search.html, modified navbar to correct homepage links.

---
+++ classes/Listing.php
@@ -0,0 +1,120 @@
+<?php
+
+class Listing
+{
+    private $_id;
+    private $_name;
+    private $_brand;
+    private $_price;
+    private $_desc;
+    private $_specs;
+
+    public function __construct($id,$name,$brand,$price,$desc,$specs)
+    {
+        $this->_id = $id;
+        $this->_name = $name;
+        $this->_brand = $brand;
+        $this->_price = $price;
+        $this->_desc = $desc;
+        $this->_specs = $specs;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->_id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId($id)
+    {
+        $this->_id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->_name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->_name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBrand()
+    {
+        return $this->_brand;
+    }
+
+    /**
+     * @param string $brand
+     */
+    public function setBrand($brand)
+    {
+        $this->_brand = $brand;
+    }
+
+    /**
+     * @return float
+     */
+    public function getPrice()
+    {
+        return $this->_price;
+    }
+
+    /**
+     * @param float $price
+     */
+    public function setPrice($price)
+    {
+        $this->_price = $price;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDesc()
+    {
+        return $this->_desc;
+    }
+
+    /**
+     * @param string $desc
+     */
+    public function setDesc($desc)
+    {
+        $this->_desc = $desc;
+    }
+
+    /**
+     * @return array
+     */
+    public function getSpecs()
+    {
+        return $this->_specs;
+    }
+
+    /**
+     * @param array $specs
+     */
+    public function setSpecs($specs)
+    {
+        $this->_specs = $specs;
+    }
+
+
+
+}
--- index.php
+++ index.php
@@ -14,20 +14,30 @@ $f3->route('GET /', function() {
     echo $view->render('views/home.html');
 });
 
-
 //Define a route for about page
 $f3->route('GET /about', function() {
     $view = new Template();
     echo $view->render('views/about.html');
 });
 
+//Define a route for searching/filtering/sorting listings
+$f3->route('GET|POST /search', function($f3) {
+    //Populate an array of test/example listings
+    $testListings = array(
+        new Listing('instr1','Test name 1', 'Test brand 1', 5, 'Test desc 1', array('spec1' => 'spec1', 'spec2'=>'spec2')),
+        new Listing('instr2','Test name 2', 'Test brand 2', 5, 'Test desc 2', array('spec1' => 'spec1', 'spec2'=>'spec2')),
+        new Listing('instr3','Test name 3', 'Test brand 3', 5, 'Test desc 3', array('spec1' => 'spec1', 'spec2'=>'spec2')),
+        new Listing('instr4','Test name 4', 'Test brand 4', 5, 'Test desc 4', array('spec1' => 'spec1', 'spec2'=>'spec2')),
+    );
+    //Set number of listings per row, and a variable for the listings
+    $f3->set('row',3);
+    $f3->set('listings', $testListings);
 
-//Define a route for about page
-$f3->route('GET /listing', function() {
     $view = new Template();
-    echo $view->render('views/listing.html');
+    echo $view->render('views/search.html');
 });
-//Define a route for any particular listing
+
+//Define a route for any particular listing/instrument's page
 $f3->route('GET /listing/@listing', function($f3, $params) {
     //TODO: Query a database using $params['listing'] to
     // get a listing object and display appropriate data
