f97781cea7a0d409cd9cfaad8c34bba543255aba
edeister <deister.ethan@student.greenriver.edu>
2024-05-25 17:23:17 -0400

    Added basic controller class with functions for each route. Added a datalayer with a test function for getting test Listing objects and a WIP function which will query a database for Listings based on an array of filters.

---
+++ controllers/controller.php
@@ -0,0 +1,52 @@
+<?php
+class Controller
+{
+    private static $_f3;
+
+    function __Construct($f3)
+    {
+        $this->_f3 = $f3;
+    }
+
+    function home()
+    {
+        $view = new Template();
+        echo $view->render('views/home.html');
+    }
+
+    function about()
+    {
+        $view = new Template();
+        echo $view->render('views/about.html');
+    }
+
+    function search()
+    {
+        //Populate an array of test/example listings;
+        $testListings = DataLayer::getListings('','');
+
+        //Set number of listings per row, and a variable for the listings
+        $this->_f3->set('row',3);
+        $this->_f3->set('listings', $testListings);
+
+        $view = new Template();
+        echo $view->render('views/search.html');
+
+    }
+
+    function listing($params)
+    {
+        //TODO: Query a database using $params['listing'] to
+        // get a listing object and display appropriate data
+
+        $view = new Template();
+        echo $view->render('views/listing.html');
+
+    }
+
+    function cart()
+    {
+        $view = new Template();
+        echo $view->render('views/cart.html');
+    }
+}
